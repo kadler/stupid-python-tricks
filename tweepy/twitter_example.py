8b4fc2195bb14eb338a4aec6dc2f322cd200d8b4
Kevin Adler <kadler@us.ibm.com>
2018-06-11 11:40:09 -0500

    Update tweepy example to use cursor and config.py instead of envvars

--- tweepy/twitter_example.py
+++ tweepy/twitter_example.py
@@ -1,21 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import tweepy
-from os import environ
+from config import *
 
-# Set up your info here: 
-CONSUMER_KEY = environ["CONSUMER_KEY"]
-CONSUMER_SECRET = environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = environ["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = environ["ACCESS_TOKEN_SECRET"]
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-results = api.search("#PowerUP18")
 
-for result in results[:5]:
-    print('@' + result.user.screen_name + ':')
-    print(result.text)
-    print()
+for tweet in tweepy.Cursor(api.search, q="#NHMUG").items(4):
+    print('@' + tweet.user.screen_name + ':')
+    print(tweet.text, "\n")
