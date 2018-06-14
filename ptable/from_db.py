420b39cafed37b1cbcebadaadeebe07de14376ac
Kevin Adler <kadler@us.ibm.com>
2018-06-14 08:57:21 -0500

    Don't hard-code sql in ptable example

--- ptable/from_db.py
+++ ptable/from_db.py
@@ -2,10 +2,11 @@
 # -*- coding: utf-8 -*-
 from prettytable import from_db_cursor
 import ibm_db_dbi as db2 
+from sys import argv
 
 conn = db2.connect()
 cur = conn.cursor()
 
-cur.execute("select cusnum, lstnam, cdtlmt, baldue, cdtdue from qiws.qcustcdt")
+cur.execute(" ".join(argv[1:]))
 
 print(from_db_cursor(cur))
